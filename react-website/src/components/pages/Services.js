8252638ef3f0c7829f4f4fcaaf43aea29f8f6c80
kendrickassignemt <72587399+kendrickassignemt@users.noreply.github.com>
2020-10-29 16:28:51 +0700

    first website

---
+++ react-website/src/components/pages/Services.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+
+export default function Services() {
+  return <h1 className='services'>SERVICES</h1>;
+}
---
+++ react-website/src/components/pages/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+export default function SignUp() {
+  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+}
